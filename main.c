e4f8495aee59b1667c5cf09026743777410b1138
bjorn <tie.372@gmail.com>
2021-02-12 08:55:09 -0700

    Add main.c example;

---
+++ main.c
@@ -0,0 +1,20 @@
+#include <openxr/openxr.h>
+#include "jsmn.h"
+#include "lox.h"
+
+int main(int argc, char** argv) {
+  XrInstance instance;
+
+  XrInstanceCreateInfo info = {
+    .type = XR_TYPE_INSTANCE_CREATE_INFO,
+    .applicationInfo = {
+      .applicationName = "test",
+      .apiVersion = XR_CURRENT_API_VERSION
+    }
+  };
+
+  XrResult result = xrCreateInstance(&info, &instance);
+  if (XR_FAILED(result)) return 1;
+  xrDestroyInstance(&instance);
+  return 0;
+}
